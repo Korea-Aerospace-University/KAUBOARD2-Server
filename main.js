7c3dca542979e958f541a3683a67dfc3e9bdc65c
infOdysseus <infodysseus@gmail.com>
2021-04-06 17:16:25 +0900

    update message

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT ? process.env.PORT : 3000
 const app = express()
 
 app.get("/", (req, res, next) => {
-    res.send({hello:"world"})
+    res.send("Hello World")
 })
 
 app.listen(PORT)
