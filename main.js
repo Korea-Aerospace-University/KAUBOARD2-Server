132b71d435c3c11dc98d5579b586ad1fec77eb67
infOdysseus <infodysseus@gmail.com>
2021-04-10 23:53:22 +0900

    change api format

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ app.get("/contents",(req, res, next)=>{
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    res.send(data['content'])
+    res.send(data)
 })
 
 
