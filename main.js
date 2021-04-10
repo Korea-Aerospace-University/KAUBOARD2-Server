8d8467ede700c5473e081e44dd0be38366df6ad8
infOdysseus <infodysseus@gmail.com>
2021-04-10 23:27:45 +0900

    add API

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const express = require('express');
-const PORT = process.env.PORT ? process.env.PORT : 3000
+const PORT = process.env.PORT ? process.env.PORT : 4000
 const app = express()
+const fs = require('fs')
+
+app.use(express.static('statics'))
+app.use(express.urlencoded({extended: false}))
+
+app.post("/submit", (req, res, next) => {
+    let data = JSON.stringify(req.body)
+    console.log(data)
+    fs.writeFileSync('contents.json', data)
+        res.redirect('/')
+    })
 
 app.get("/", (req, res, next) => {
-    res.send("Hello World")
+    res.sendFile(__dirname + '/statics/html/home_notice_upload.html')
 })
+
+app.get("/contents",(req, res, next)=>{
+    let rawData = fs.readFileSync('contents.json')
+    let data = JSON.parse(rawData)
+    res.send(data['content'])
+})
+
 
 app.listen(PORT)
---
+++ statics/js/update_db.js
@@ -0,0 +1,3 @@
+document.querySelector('.form').addEventListener('submit', (e) => {
+    console.log(e)
+})
