1f04479844a709d6113549c40657a035195821ee
park hyunbin <wise7231@gmail.com>
2021-05-12 21:19:42 +0900

    nodeJs 템플릿 업로드

---
+++ api-server-node-js-final 2/src/app/User/userController.js
@@ -0,0 +1,94 @@
+const jwtMiddleware = require("../../../config/jwtMiddleware");
+const userProvider = require("../../app/User/userProvider");
+const userService = require("../../app/User/userService");
+const baseResponse = require("../../../config/baseResponseStatus");
+const {response, errResponse} = require("../../../config/response");
+
+const regexEmail = require("regex-email");
+const {emit} = require("nodemon");
+
+/**
+ * API No. 0
+ * API Name : 테스트 API
+ * [GET] /app/test
+ */
+// exports.getTest = async function (req, res) {
+//     return res.send(response(baseResponse.SUCCESS))
+// }
+
+/**
+ * API No. 1
+ * API Name : 유저 생성 (회원가입) API
+ * [POST] /app/users
+ */
+exports.postUsers = async function (req, res) {
+
+    /**
+     * Body: email, password, nickname
+     */
+    const {email, password, nickname} = req.body;
+
+    // 빈 값 체크
+    if (!email)
+        return res.send(response(baseResponse.SIGNUP_EMAIL_EMPTY));
+
+    // 길이 체크
+    if (email.length > 30)
+        return res.send(response(baseResponse.SIGNUP_EMAIL_LENGTH));
+
+    // 형식 체크 (by 정규표현식)
+    if (!regexEmail.test(email))
+        return res.send(response(baseResponse.SIGNUP_EMAIL_ERROR_TYPE));
+
+    // 기타 등등 - 추가하기
+
+
+    const signUpResponse = await userService.createUser(
+        email,
+        password,
+        nickname
+    );
+
+    return res.send(signUpResponse);
+};
+
+/**
+ * API No. 2
+ * API Name : 유저 조회 API (+ 이메일로 검색 조회)
+ * [GET] /app/users
+ */
+exports.getUsers = async function (req, res) {
+
+    /**
+     * Query String: email
+     */
+    const email = req.query.email;
+
+    if (!email) {
+        // 유저 전체 조회
+        const userListResult = await userProvider.retrieveUserList();
+        return res.send(response(baseResponse.SUCCESS, userListResult));
+    } else {
+        // 유저 검색 조회
+        const userListByEmail = await userProvider.retrieveUserList(email);
+        return res.send(response(baseResponse.SUCCESS, userListByEmail));
+    }
+};
+
+/**
+ * API No. 3
+ * API Name : 특정 유저 조회 API
+ * [GET] /app/users/{userId}
+ */
+exports.getUserById = async function (req, res) {
+
+    /**
+     * Path Variable: userId
+     */
+    const userId = req.params.userId;
+
+    if (!userId) return res.send(errResponse(baseResponse.USER_USERID_EMPTY));
+
+    const userByUserId = await userProvider.retrieveUser(userId);
+    return res.send(response(baseResponse.SUCCESS, userByUserId));
+};
---
+++ api-server-node-js-final 2/src/app/User/userProvider.js
@@ -0,0 +1,40 @@
+const { pool } = require("../../../config/database");
+const { logger } = require("../../../config/winston");
+
+const userDao = require("./userDao");
+
+// Provider: Read 비즈니스 로직 처리
+
+exports.retrieveUserList = async function (email) {
+  if (!email) {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const userListResult = await userDao.selectUser(connection);
+    connection.release();
+
+    return userListResult;
+
+  } else {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const userListResult = await userDao.selectUserEmail(connection, email);
+    connection.release();
+
+    return userListResult;
+  }
+};
+
+exports.retrieveUser = async function (userId) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const userResult = await userDao.selectUserId(connection, userId);
+
+  connection.release();
+
+  return userResult[0];
+};
+
+exports.emailCheck = async function (email) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const emailCheckResult = await userDao.selectUserEmail(connection, email);
+  connection.release();
+
+  return emailCheckResult;
+};
