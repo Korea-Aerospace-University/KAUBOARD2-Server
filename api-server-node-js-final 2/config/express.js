22bbbdb57b0105dbc42387cdfdbf0afadaf52860
suyeoniii <ssy4230@naver.com>
2021-07-07 19:04:17 +0900

    express 수정

--- api-server-node-js-final 2/config/express.js
+++ api-server-node-js-final 2/config/express.js
@@ -8,30 +8,11 @@ module.exports = function () {
 
     const app = express();
 
-    app.use(compression());
-
-    app.use(express.json());
-    app.use(cookieParser());
-
-    //ejs
-    app.set('view engine', 'ejs');
-    app.set('views', __dirname + '/views');
-
-    app.use(express.urlencoded({ extended: true }));
-
-    app.use(methodOverride());
-
-    app.use(cors());
-    app.use(express.static("statics"));
-
-
     //var server = app.listen(3000)
     const httpServer = http.createServer(app, {
         cors: {
-          origin: "*",
-          methods: ["GET", "POST"],
-        origin: "*",
-        methods: ["GET", "POST"],
+            origin: "*",
+            methods: ["GET", "POST"],
         },
       })
 
@@ -51,6 +32,22 @@ module.exports = function () {
       io.emit("message", formatMessage(data.name, data.msg));
     });
   });
+
+    app.use(compression());
+
+    app.use(express.json());
+    app.use(cookieParser());
+
+    //ejs
+    app.set('view engine', 'ejs');
+    app.set('views', __dirname + '/views');
+
+    app.use(express.urlencoded({ extended: true }));
+
+    app.use(methodOverride());
+
+    app.use(cors());
+    app.use(express.static("statics"));
 
     // TODO: 도메인을 추가할 경우 이곳에 Route를 추가하세요.
     require("../src/app/User/userRoute")(app);
